ec1b2a2288ef816f18d79b2b9f625987f1a78737
Yago Pajariño <yagopajarino@outlook.com>
2022-08-24 15:06:37 -0300

    Add Donaciones component

---
+++ src/components/Donaciones.js
@@ -0,0 +1,15 @@
+import Cafecito from "./Cafecito";
+
+export default function Donaciones() {
+  return (
+    <div className=" bg-yellow-300 flex items-center justify-around">
+      <div className="w-3/4 p-5 flex items-center justify-between">
+        <div className="flex flex-col">
+          <h2 className="text-3xl">Donaciones</h2>
+          <p>Tus cafecitos ayudan a mantener la app.</p>
+        </div>
+        <Cafecito />
+      </div>
+    </div>
+  );
+}
