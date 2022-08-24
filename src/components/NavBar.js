a72b12420cbe8252bebeaf6adec9530f4fef77e3
Yago Pajariño <yagopajarino@outlook.com>
2022-08-24 15:05:48 -0300

    Add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <nav className="w-full flex flex-col items-center border-b backdrop-blur">
+      <div className="w-3/4 flex items-center ">
+        <Link to="/">
+          <h1 className="text-4xl py-4">MLMV</h1>
+        </Link>
+      </div>
+    </nav>
+  );
+}
