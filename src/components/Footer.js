ef7bed72f3d3a1c74050e288f46b550b74d3a46d
Yago Pajariño <yagopajarino@outlook.com>
2022-08-24 15:06:13 -0300

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className=" bg-slate-900 text-slate-50">
+      <div className="w-full flex p-5 items-center justify-around">
+        <p>Yago Pajariño</p>
+      </div>
+    </footer>
+  );
+}
