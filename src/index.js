3272b13838a2309f516b1cd0e60836bccd479e7c
Yago Pajariño <yagopajarino@outlook.com>
2022-09-23 14:52:22 -0300

    Add highscore route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import Home from "./rutas/Home";
 import Game from "./rutas/Game";
 import Puntajes from "./rutas/Puntajes";
+import Highscore from "./rutas/Highscore";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,6 +15,7 @@ root.render(
       <Route path="/" element={<App />}>
         <Route path="/" element={<Home />} />
         <Route path="/jugar" element={<Game />} />
+        <Route path="/highscore" element={<Highscore />} />
         <Route path="/puntajes" element={<Puntajes />} />
       </Route>
     </Routes>
---
+++ src/rutas/Highscore.js
@@ -0,0 +1,3 @@
+export default function Highscore() {
+  return <h1>Nuevo puntaje alto</h1>;
+}
