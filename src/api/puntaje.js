77ae65980c0cfcd776843e742899640f1e97202c
Yago Pajariño <yagopajarino@outlook.com>
2022-09-23 14:51:41 -0300

    Add puntaje api controller

---
+++ src/api/puntaje.js
@@ -0,0 +1,14 @@
+const getPuntajeRonda = (ronda) => {
+  return 10;
+};
+
+const esPuntajeAlto = (puntaje) => {
+  return false;
+};
+
+const puntaje = {
+  getPuntajeRonda,
+  esPuntajeAlto,
+};
+
+export default puntaje;
