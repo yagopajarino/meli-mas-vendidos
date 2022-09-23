0efcac0be95b4ace3dad20272cdc7153f16f6e41
Yago Pajariño <yagopajarino@outlook.com>
2022-09-23 14:52:05 -0300

    Add metodos to api class

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,13 @@
 import categories from "./categories";
 import products from "./products";
+import puntaje from "./puntaje";
+import partida from "./partida";
 
 const api = {
   categories,
   products,
+  puntaje,
+  partida,
 };
 
 export default api;
