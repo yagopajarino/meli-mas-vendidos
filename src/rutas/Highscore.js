5bbf97d48ad7047a4470c9f171976c302d64bd7b
Yago Pajariño <yagopajarino@outlook.com>
2022-10-09 13:20:14 -0300

    Add Highscore route component

--- src/rutas/Highscore.js
+++ src/rutas/Highscore.js
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import api from "../api/api";
+
 export default function Highscore() {
-  return <h1>Nuevo puntaje alto</h1>;
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+
+  const { state } = useLocation();
+  const { puntaje, userID } = state; // Read values passed on state
+
+  const savePuntaje = (e) => {
+    const data = {
+      user_id: userID,
+      username: name,
+      timestamp: new Date().getTime(),
+      puntaje: puntaje,
+    };
+    api.puntaje.savePuntajeAlto(data);
+    navigate("/puntajes");
+  };
+
+  return (
+    <div>
+      <h1>Nuevo puntaje alto</h1>
+      <h2>
+        Felicitaciones! Obtuviste uno de los 10 puntajes más altos del juego
+      </h2>
+      <form>
+        <input
+          placeholder="Nombre"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>
+        <h2>{puntaje}</h2>
+      </form>
+      <div onClick={savePuntaje}>Guardar</div>
+    </div>
+  );
 }
