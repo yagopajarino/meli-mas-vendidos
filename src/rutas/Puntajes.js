18f967fa3328956a02eb3efedf58ce3ef2614322
Yago Pajariño <yagopajarino@outlook.com>
2022-10-09 13:20:36 -0300

    Add Puntaje route component

--- src/rutas/Puntajes.js
+++ src/rutas/Puntajes.js
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+import api from "../api/api";
+
 export default function Puntajes() {
-  return <h1>Puntajes</h1>;
+  const [puntajes, setPuntajes] = useState([]);
+
+  useEffect(() => {
+    console.log(puntajes);
+  }, [puntajes]);
+
+  useEffect(() => {
+    api.puntaje.getPuntajesAltos().then((r) => setPuntajes(r));
+  }, []);
+
+  return (
+    <div>
+      <h1>Puntajes</h1>
+      {puntajes.length == 0
+        ? ""
+        : puntajes.map((i) => {
+            return (
+              <div>
+                <p>{i.username}</p>
+                <p>{i.puntaje}</p>
+              </div>
+            );
+          })}
+    </div>
+  );
 }
