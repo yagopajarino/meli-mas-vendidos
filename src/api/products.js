713a595834ce5f13b06572bef0fdf9777a8298ae
Yago Pajariño <yagopajarino@outlook.com>
2022-10-09 13:18:04 -0300

    Update products api module

--- src/api/products.js
+++ src/api/products.js
@@ -1,11 +1,18 @@
 const getProducts = async (categoryId) => {
   const url = `${process.env.REACT_APP_API_ENDPOINT}/productos?id=${categoryId}`;
   const data = await fetch(url).then((r) => r.json());
+  return data.map((i) => i.product);
+};
+
+const getMasVendidos = async (categoryId) => {
+  const url = `${process.env.REACT_APP_API_ENDPOINT}/productos?id=${categoryId}`;
+  const data = await fetch(url).then((r) => r.json());
   return data;
 };
 
 const products = {
   getProducts,
+  getMasVendidos,
 };
 
 export default products;
