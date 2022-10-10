27048c03c28ff29783c655ac99f63f8a847a3ac7
Yago Pajariño <yagopajarino@outlook.com>
2022-10-10 20:29:59 -0300

    Change categories api call

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,6 +1,5 @@
 const getCategories = async () => {
   const url = `${process.env.REACT_APP_API_ENDPOINT}/categorias/`;
-  console.log(url);
   const response = await fetch(url).then((r) => r.json());
   return response;
 };
