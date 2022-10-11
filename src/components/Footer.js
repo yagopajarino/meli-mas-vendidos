1c2b699b16857161c430b36a2bf03b3fa92841ea
Yago Pajariño <yagopajarino@outlook.com>
2022-10-11 12:04:45 -0300

    Change footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
 export default function Footer() {
   return (
     <footer className=" bg-slate-900 text-slate-50">
-      <div className="w-full flex p-5 items-center justify-around">
-        <p>Yago Pajariño</p>
+      <div className="w-full flex p-5 items-center justify-center">
+        <a href="https://yagopajarino.ar" className="px-2">
+          Yago Pajariño
+        </a>
+        |<p className="px-2">{new Date().getFullYear()}</p>
       </div>
     </footer>
   );
