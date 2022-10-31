2b86632f2e36436573092f9e908be41908929d31
Yago Pajariño <yagopajarino@outlook.com>
2022-10-31 14:46:06 -0300

    Change react router basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/meli-mas-vendidos">
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="/" element={<Home />} />
