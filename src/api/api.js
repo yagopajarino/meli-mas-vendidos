92b0a6d6063bf5adfb845672cec4a355dd5a1fa3
Yago Pajariño <yagopajarino@outlook.com>
2022-11-08 11:22:02 -0300

    Add users object to api controller

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,14 @@ import categories from "./categories";
 import products from "./products";
 import puntaje from "./puntaje";
 import partida from "./partida";
+import users from "./users";
 
 const api = {
   categories,
   products,
   puntaje,
   partida,
+  users,
 };
 
 export default api;
