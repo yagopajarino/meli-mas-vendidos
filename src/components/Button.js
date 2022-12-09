22c1f9d25a67659160f2fa21956ef23933aca32c
Yago Pajariño <yagopajarino@outlook.com>
2022-12-08 21:22:31 -0300

    Change Button font size

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 export default function Button({ children, onClick, disabled }) {
   return (
-    <button disabled={disabled} onClick={onClick}>
+    <button disabled={disabled} onClick={onClick} className="text-lg">
       {children}
     </button>
   );
